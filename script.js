4d2c75df59cd7bb53e3178630250fa283755fef3
Michael Werner <reznaeous@gmail.com>
2025-10-24 04:12:44 -0400

    Add remove item function

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
+const clearAll = document.getElementById('clear');
 
 function addItem(e) {
   e.preventDefault();
@@ -37,4 +38,14 @@ function createIcon(classes) {
   return icon;
 }
 
+function removeItem(e) {
+  if (e.target.parentElement.classList.contains('remove-item')) {
+    e.target.parentElement.parentElement.remove();
+  }
+}
+
+function clearItems(e) {}
+
 itemForm.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+clearAll.addEventListener('click', clearItems);
